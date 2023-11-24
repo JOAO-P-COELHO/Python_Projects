ea27e3b9e5663250b0bb1295ea151b918c063c4c
JOAO-P-COELHO <143346573+JOAO-P-COELHO@users.noreply.github.com>
2023-11-24 19:28:17 +0000

    finished

--- BankingAppV2.py
+++ BankingAppV2.py
@@ -1,48 +1,41 @@
 # Banking App - Final project
 
-while True:
-    transaction = open("transaction_history.txt", "a")
+class Banking_App:
     
-    class Banking_App:
-
-        def __init__(self, withdrawl_value=0.00, deposit_value=0.00):
-            
-            withdrawl_value=0.00
-            deposit_value=0.00
-            balance=0.00
-            
-            
-
-            operation = input("What kind of operation you would like to do: Withdrawl (W) or Deposit?(D)?  ")
+    balance_value = 0.00
+                        
+    def __init__(self):  
+        while True:
+            operation = input("\nWhat kind of operation you would like to do: Withdrawl (W) or Deposit?(D)? Press (L) to leave.   ")
             operation = operation.lower()
-
             if operation == "withdrawl" or operation =="w" :
                 self.withdrawl()
             elif operation == "deposit" or operation =="d":
-                self.deposit()        
+                self.deposit()   
+            elif operation == "leave" or operation =="l":
+                break          
             else:
                 print("You didn't choose a valid operation")
-        
-            balance = withdrawl_value + deposit_value
-            print(balance)
-                                    
-        def withdrawl(self):
-                withdrawl_value = input("What it's the value? (Use CTRL+C to stop this loop) ")
-                withdrawl_value = int(withdrawl_value)
-                transaction.write(f"The withdrawl value is: {withdrawl_value}$\n")
-                                                              
-                self.__init__(withdrawl_value)
-                return withdrawl_value
-        
-        def deposit(self):
-                deposit_value = input("What it's the value?  (Use CTRL+C to stop this loop) ")
-                deposit_value = int(deposit_value)
-                transaction.write(f"The deposit value is: {deposit_value}$\n")
-                
-                self.__init__(deposit_value)
-                return deposit_value
-            
-    app = Banking_App()   
+
+    def deposit(self):
+        deposit_value = input("What it's the value? ")
+        deposit_value = float(deposit_value)
+        with open("transaction_history.txt", "a") as transaction:
+            transaction.write(f"The deposit value is: {deposit_value}$\n")
+            self.balance_value += deposit_value
+            transaction.write(f"Your balance is now: {self.balance_value}$\n\n")
+            print(f"Your balance is now: {self.balance_value}$")    
+                                           
+    def withdrawl(self):
+        withdrawl_value = input("What it's the value? ")
+        withdrawl_value = float(withdrawl_value)
+        with open("transaction_history.txt", "a") as transaction:
+            transaction.write(f"The withdrawl value is: {withdrawl_value}$\n")
+            self.balance_value -= withdrawl_value
+            transaction.write(f"Your balance is now: {self.balance_value}$\n\n")
+            print(f"Your balance is now: {self.balance_value}$") 
+                               
+app = Banking_App()   
 
     
 
