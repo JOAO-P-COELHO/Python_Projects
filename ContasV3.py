7c8867d61570e2b5276fb041118994ad6ae869db
JOAO-P-COELHO <143346573+JOAO-P-COELHO@users.noreply.github.com>
2023-12-15 15:25:32 +0000

    s

--- ContasV3.py
+++ ContasV3.py
@@ -1,5 +1,9 @@
 def arithmetic_arranger (array, show=False):
-
+    array_len = len(array)
+    
+    if array_len>5:
+        return "Error: Too many problems."
+    
     def split(array):
         result = array.split(' ')
         return result
@@ -8,19 +12,38 @@ def arithmetic_arranger (array, show=False):
                                         # map(function, iterables)
                                         # The list converts the map into a list, otherwise it would return something like <map object at 0x034244F0
 
-    array_len = len(array)
+    i=0
+    while i<array_len:
+        if resultado[i][1] != "+" and resultado[i][1] != "-":
+            return("Error1: Operator must be '+' or '-'")
+        i = i + 1 
 
     i=0
+    while i<array_len:
+        if resultado[i][0].isnumeric() and resultado[i][2].isnumeric():
+            pass
+        else:
+            return("Error: Numbers must only contain digits.")
+        i = i + 1
+        
+    i=0
+    while i<array_len:
+        if len(resultado[i][0])<=4 and len(resultado[i][2])<=4:
+            pass
+        else:
+            return("Error: Numbers cannot be more than four digits.")
+        i = i + 1
+    
+    i=0 # Primeiro número
     while i<array_len:
         print(resultado[i][0].rjust(7, ' '), end=" ")
         i = i + 1
-
     print("")
-    i=0
+    
+    i=0# Sinal e segundo número
     while i<array_len:
         print(resultado[i][1].ljust(0, ' '), resultado[i][2].rjust(5, ' '), end=" ", )
         i = i + 1
-
     print ("")
     
     i=0
@@ -30,7 +53,7 @@ def arithmetic_arranger (array, show=False):
     
     print ("")
     i=0
-    while i<array_len:
+    while i<array_len:        
         if show==True and resultado[i][1] == "+":
             operacao = int(resultado[i][0]) + int(resultado[i][2])
             print(str(operacao).rjust(7, " "), end=" ")
@@ -38,10 +61,8 @@ def arithmetic_arranger (array, show=False):
             operacao = int(resultado[i][0]) - int(resultado[i][2])
             print(str(operacao).rjust(7, " "), end=" ")
     
-        i = i + 1
-    
-    
-    # print(array_len * "----- ")
+        i = i + 1 
+
 # def raciciocio_matematico:
     # [['32' , '+'    , '698'] , ['3801',  '-'   ,  '2']  , ['45'  ,  '+'   , '43']]
     # [0][0] , [0][1] , [0][2] , [1][0] , [1][1] , [1][2] , [2][0] , [2][1] , [2][2]
@@ -51,4 +72,6 @@ def arithmetic_arranger (array, show=False):
     # [0][1] [1][1] [2][1]
     # [0][2] [1][2] [2][2]
 
-arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
+x = arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True)
+
+# print("\n this is returning:", x)
