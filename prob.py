69f0613610f785690c20a98ee26ea9ced2057fd2
JOAO-P-COELHO <143346573+JOAO-P-COELHO@users.noreply.github.com>
2024-01-16 18:35:53 +0000

    Create prob.py

---
+++ prob.py
@@ -0,0 +1,54 @@
+
+# class Hat(red=0, orange=0, black=0, blue=0, pink=0, striped=0):
+    
+#     def __init__(self):
+#         self.contents = []
+
+
+class Hat:
+    contents = []
+    x=0
+    
+    def __init__(self):
+        self.red=red
+        self.orange=orange
+        self.black=black
+        self.blue=blue
+        self.pink=pink
+        self.striped=striped
+        print(red)
+    
+    # while x < red:
+    #     contents.append("red")
+    #     x = x + 1
+    # x=0
+    
+    # while x < orange:
+    #     contents.append("orange")
+    #     x = x + 1
+    # x=0
+    
+    # while x < black:
+    #     contents.append("black")
+    #     x = x + 1
+    # x=0    
+    
+    # while x < blue:
+    #     contents.append("blue")
+    #     x = x + 1
+    # x=0   
+    
+    # while x < pink:
+    #     contents.append("pink")
+    #     x = x + 1
+    # x=0 
+        
+    # while x < striped:
+    #     contents.append("striped")
+    #     x = x + 1
+    # print(contents)
+    # x=0 
+    
+
+x = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
+print(x)
