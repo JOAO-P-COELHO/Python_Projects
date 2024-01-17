e1ba4e5722fa7a0e2dbdc5c395ca6ba9a0197e88
JOAO-P-COELHO <143346573+JOAO-P-COELHO@users.noreply.github.com>
2024-01-17 18:37:49 +0000

    .

--- testes.py
+++ testes.py
@@ -1,11 +1,16 @@
-class Pessoa:
-    def __init__(self, nome, idade):
-        self.nome = nome
-        self.idade = idade
+def experiment(hat=0, expected_balls=0, num_balls_drawn=0, num_experiments=0): 
 
-    def apresentacao(self):
-        print(f"{self.nome} tem: {self.idade}")
-
-
-pessoa1 = Pessoa("João", 30)
-pessoa1.apresentacao()
+    dict_to_list = []
+    
+    list_expected_balls = expected_balls
+    for element in list_expected_balls:
+        number_reps = list_expected_balls[element]
+        while number_reps > 0:
+            dict_to_list.append(element)
+            number_reps -= 1
+    
+    print(dict_to_list)
+        
+            
+            
+probability = experiment(expected_balls = {"red":2,"green":1})
